ecb0e0e999f5e059e9da563ea43ea32e91799d14
ammarzakwan-cell <ammaramran01@gmail.com>
2025-01-16 08:25:47 +0800

    update ui form

--- modules/task/views.py
+++ modules/task/views.py
@@ -5,41 +5,46 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from django.db.models import Q
-from .models import Task, Locking
+from .models import Task, Locking 
 from modules.media.models import Media
-from .forms import TaskForm
 from components.storage_component import StorageComponent
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.decorators import login_required, permission_required
 from django.views.decorators.csrf import csrf_exempt
+from datetime import datetime
+from django.utils.timezone import make_aware
 
 # Create your views here.
 @login_required(login_url="/login")
 def task_index(request):
-    return render(request, 'task_index.html')
+    user_groups = request.user.groups.values_list('name', flat=True)
+    return render(request, 'task_index.html', {'user_groups': user_groups})
 
 @login_required(login_url="/login")
 @permission_required("task.add_task", login_url="/login", raise_exception=True)
 def task_create(request):
     if request.method == 'POST':
-        form = TaskForm(request.POST, request.FILES)
+            
+        # Save the Task instance
+        task = Task.objects.create(
+            title=request.POST.get('title'),
+            source=request.POST.get('source'),
+            content=request.POST.get('content'),
+            published=make_aware(datetime.strptime(request.POST.get('date'), "%Y-%m-%d")) if request.POST.get('date') else None
+        )
 
-        if form.is_valid():
-            # Save the Task instance
-            task = form.save(commit=False)
-            task.save()
 
-            # Upload the file and associate it with the Task
-            file = form.cleaned_data['file']
+        # Upload the file and associate it with the Task
+        # Check if a new file is uploaded
+        if 'file' in request.FILES:
+            file = request.FILES['file']
             storage = StorageComponent()
             storage.disk('s3').upload_file(file, model_instance=task, collection_name="data_entry_task")
 
-            return redirect('task_index')  # Redirect to a task list page
-        else:
-            return render(request, 'task_create.html', {'form': form, 'errors': form.errors})
-    else:
-        form = TaskForm()
-    return render(request, 'task_create.html', {'form': form})
+        return redirect('task_index')  # Redirect to a task list page
+    
+    return render(request, 'task_create.html')
+
 
 @login_required(login_url="/login")
 def task_update(request, task_id):
@@ -63,36 +68,47 @@ def task_update(request, task_id):
         object_id=task.id
     ).first()
 
+    print(existing_file, task.id)
+
     if request.method == 'POST':
-        form = TaskForm(request.POST, request.FILES, instance=task)
 
-        if form.is_valid():
-            # Save updated Task instance
-            task = form.save(commit=False)
-            task.save()
+        # Save updated Task instance
+        task.title = request.POST.get('title')
+        task.source = request.POST.get('source')
+        task.content = request.POST.get('content')
+        task.published = make_aware(datetime.strptime(request.POST.get('date'), "%Y-%m-%d"))
+        task.save()
 
-            # Check if a new file is uploaded
-            if 'file' in request.FILES:
-                file = form.cleaned_data['file']
+        # Check if a new file is uploaded
+        if 'file' in request.FILES:
+            file = request.FILES['file']
 
-                # Delete the old file from S3
-                if existing_file and storage.is_exist(existing_file.file_path):
-                    storage.remove(existing_file.file_path)
+            # Delete the old file from S3
+            if existing_file and storage.is_exist(existing_file.file_path):
+                storage.remove(existing_file.file_path)
 
-                storage.upload_file(
-                    file, model_instance=task, collection_name="data_entry_task"
-                )
+            storage.upload_file(
+                file, model_instance=task, collection_name="data_entry_task"
+            )
 
-            return redirect('task_index')  # Redirect to a task list page
-        else:
-            return render(request, 'task_update.html', {'form': form, 'errors': form.errors, 'task': task})
+        return redirect('task_index')  # Redirect to a task list page
     else:
-        form = TaskForm(instance=task)
+        user_groups = request.user.groups.values_list('name', flat=True)
+        image_url = None
+        if existing_file:
+            image_url = storage.generate_signed_url(existing_file.file_path)
+        return render(request, 'task_update.html', {
+            'task': task, 
+            'image_url': image_url, 
+            'existing_file': existing_file, 
+            'user_groups': user_groups
+            })
+
+
     
-    image_url = storage.generate_signed_url(existing_file.file_path)
 
-    return render(request, 'task_update.html', {'form': form, 'task': task, 'image_url': image_url, 'existing_file': existing_file})
 
+@login_required(login_url="/login")
 @csrf_exempt
 def unlock_task(request):
     if request.method == "POST":
